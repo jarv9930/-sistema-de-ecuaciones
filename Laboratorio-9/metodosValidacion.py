86d954e2de9ceec73d24c5e2cb0337e1c7424ece
Rodrigo <rodrigo77e@outlook.com>
2024-05-10 22:46:33 -0600

    Hold out para todos los datasets

--- Laboratorio-9/metodosValidacion.py
+++ Laboratorio-9/metodosValidacion.py
@@ -1,11 +1,10 @@
 import numpy as np
-import random
 import pandas as pd
 
 # Definir funciones para Hold Out 70/30 estratificado
-def hold_out_70_30_stratified(X, y):
+def hold_out_70_30_estratificado(X, y):
     unique_classes = np.unique(y)
-    #print("Clases únicas: ", unique_classes)
+    print("Clases únicas: ", unique_classes)
     train_indices = []
     test_indices = []
     for cls in unique_classes:
@@ -19,41 +18,62 @@ def hold_out_70_30_stratified(X, y):
     y_train, y_test = y[train_indices], y[test_indices]
     return X_train, X_test, y_train, y_test
 
+def aplicar_hold_out_70_30_estratificado(dataset,carpeta,class_column=-1):
+    
+    # Aplicar métodos de validación en conjuntos de datos
+
+    if class_column == -1:
+        X = dataset.iloc[:,:-1].to_numpy()
+        y = dataset.iloc[:, -1].values
+    else:
+        todas_las_columnas = dataset.columns
+        columnas = todas_las_columnas.drop(class_column)
+        X = dataset[columnas].to_numpy()
+        y = dataset[class_column].values
+    print("X: ", X)
+    print("y: ", y)
+    dataset_hold_out = hold_out_70_30_estratificado(X, y)
+
+    #Imprimir conjuntos de prueba y entrenamiento
+    indice = 0
+    print("Conjunto de entrenamiento: ")
+    for i in dataset_hold_out[0]:
+        print(i, "->", dataset_hold_out[2][indice])
+        indice += 1
+    indice = 0
+    print("Conjunto de prueba: ")
+    for i in dataset_hold_out[1]:
+        print(i, "->", dataset_hold_out[3][indice])
+        indice += 1
+    
+
+    #Guardar conjuntos de entrenamiento y prueba en archivos CSV
+    if class_column == -1:
+        dataset_train = pd.DataFrame(dataset_hold_out[0],columns=dataset.columns[:-1])
+        dataset_train[dataset.columns[-1]] = dataset_hold_out[2]
+        dataset_train.to_csv(carpeta+"entrenamiento.csv", index=False)
+
+        dataset_test = pd.DataFrame(dataset_hold_out[1],columns=dataset.columns[:-1])
+        dataset_test[dataset.columns[-1]] = dataset_hold_out[3]
+        dataset_test.to_csv(carpeta+"prueba.csv", index=False)
+    else:
+        columnas = dataset.columns.drop(class_column)
+        dataset_train = pd.DataFrame(dataset_hold_out[0],columns=columnas)
+        dataset_train[class_column] = dataset_hold_out[2]
+        dataset_train.to_csv(carpeta+"entrenamiento.csv", index=False)
+
+        dataset_test = pd.DataFrame(dataset_hold_out[1],columns=columnas)
+        dataset_test[class_column] = dataset_hold_out[3]
+        dataset_test.to_csv(carpeta+"prueba.csv", index=False)
 
 
 # Cargar conjuntos de datos
 iris = pd.read_csv("./Laboratorio-9/irisDatasets/iris.csv")
+aplicar_hold_out_70_30_estratificado(iris,"./Laboratorio-9/irisDatasets/")
 
+wine = pd.read_csv("./Laboratorio-9/wineDatasets/wine.csv")
+aplicar_hold_out_70_30_estratificado(wine,"./Laboratorio-9/wineDatasets/","Wine")
 
-indices = list(iris.index)
-random.shuffle(indices)
+cars = pd.read_csv("./Laboratorio-9/carsDatasets/cars.csv")
+aplicar_hold_out_70_30_estratificado(cars,"./Laboratorio-9/carsDatasets/")
 
-iris_random = iris.iloc[indices]
-print("Iris random: ", iris_random)
-#iris_random.to_csv("./Laboratorio-9/iris_random.csv", index=False)
-
-# Aplicar métodos de validación en conjuntos de datos
-X = iris_random.iloc[:,:-1].to_numpy()
-#print("X: ", X)
-y = iris_random.iloc[:, -1].values
-#print("y: ", y)
-iris_hold_out = hold_out_70_30_stratified(X, y)
-#Imprimir conjuntos de prueba y entrenamiento
-indice = 0
-print("Conjunto de entrenamiento: ")
-for i in iris_hold_out[0]:
-    print(i, "->", iris_hold_out[2][indice])
-    indice += 1
-indice = 0
-print("Conjunto de prueba: ")
-for i in iris_hold_out[1]:
-    print(i, "->", iris_hold_out[3][indice])
-    indice += 1
-
-#Guardar conjuntos de entrenamiento y prueba en archivos CSV
-iris_train = pd.DataFrame(iris_hold_out[0],columns=iris.columns[:-1])
-iris_train[iris.columns[-1]] = iris_hold_out[2]
-iris_train.to_csv("./Laboratorio-9/irisDatasets/iris_entrenamiento.csv", index=False)
-iris_test = pd.DataFrame(iris_hold_out[1],columns=iris.columns[:-1])
-iris_test[iris.columns[-1]] = iris_hold_out[3]
-iris_test.to_csv("./Laboratorio-9/irisDatasets/iris_prueba.csv", index=False)
